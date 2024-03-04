7a4ab582ee69b29414078aefc920b8173b7b996b
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2024-03-04 15:52:06 +0530

    Updated Solution - GfG to GitHub

--- Easy/Swap the array elements/swap-the-array-elements.java
+++ Easy/Swap the array elements/swap-the-array-elements.java
@@ -39,9 +39,9 @@ class Solution
     {
         // Code here
         for(int i=0; i<n-2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[i+2];
-            arr[i+2] = temp;
+            arr[i]=arr[i]^arr[i+2];
+            arr[i+2]=arr[i]^arr[i+2];
+            arr[i]=arr[i]^arr[i+2];
         }
     }
 }
