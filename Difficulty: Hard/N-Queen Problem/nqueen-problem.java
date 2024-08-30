ab6883c76baed1c65b142535567872a46848f47a
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2024-08-30 15:23:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.nQueen(n);
+            if (ans.size() == 0)
+                System.out.println("-1");
+            else {
+                for (int i = 0; i < ans.size(); i++) {
+                    System.out.print("[");
+                    for (int j = 0; j < ans.get(i).size(); j++)
+                        System.out.print(ans.get(i).get(j) + " ");
+                    System.out.print("] ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean isSafe(int i, int j, int m[][], int n) {
+        for(int x = i; x >= 0; x--) {
+            if(m[x][j] == 1) return false;
+        }
+        for(int x = i, y = j; x >= 0 && y >= 0; x--, y--) {
+            if(m[x][y] == 1) return false;
+        }
+        for(int x = i, y = j; x >= 0 && y < n; x--, y++) {
+            if(m[x][y] == 1) return false;
+        }
+        return true;
+    }
+    public void placeQueens(int i, int[][] m, int n, ArrayList<ArrayList<Integer>> ans) {
+        if(i == n) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int x = 0; x < n; x++) {
+                for(int y = 0; y < n; y++) {
+                    if(m[x][y] == 1) {
+                        temp.add(y+1);
+                    }
+                }
+            }
+            ans.add(temp);
+            return;
+            
+        }
+        for(int j = 0; j < n; j++) {
+            if(isSafe(i, j, m, n)) {
+                m[i][j] = 1;
+                placeQueens(i+1, m, n, ans);
+                m[i][j] = 0;
+            }
+        }
+    }
+    public ArrayList<ArrayList<Integer>> nQueen(int n) {
+        // code 
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int m[][] = new int[n][n];
+        placeQueens(0, m, n, ans);
+        return ans;
+    }
+}
