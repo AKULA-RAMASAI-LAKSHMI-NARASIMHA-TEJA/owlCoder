4636f72c674a5c971821c4789bae16e781cce706
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2024-10-01 19:39:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Multiply two linked lists/multiply-two-linked-lists.java
+++ Difficulty: Easy/Multiply two linked lists/multiply-two-linked-lists.java
@@ -25,23 +25,20 @@ class Node
 }*/
 
 class Solution {
-    public long multiplyTwoLists(Node l1, Node l2) {
+    long mod = 1000000007;
+    public long multiplyTwoLists(Node first, Node second) {
         // Code here
-        long m = 1000000007;
-          long n1 =0, n2=0;
-
-          Node temp = l1;
-          while(temp!=null){
-              n1 = (n1*10)%m+temp.data;
-              temp = temp.next;
-          }
-          temp = l2;
-          while(temp!=null){
-              n2 = (n2*10)%m + temp.data;
-              temp = temp.next;
-          }
-
-          return (n1*n2) % m;
+        long f = getNum(first);
+        long s = getNum(second);
+        return (f * s)%mod;
+    }
+    private long getNum(Node node){
+        long res = 0;
+        while(node != null){
+            res = ((res * 10) + node.data)%mod;
+            node = node.next;
+        }
+        return res;
     }
 }
 
